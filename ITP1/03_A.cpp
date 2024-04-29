77c9dea3095dfafae3b8ff64e92e7b1fc0db068c
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-04-29 10:56:23 +0900

    Create 03_A.cpp

---
+++ ITP1/03_A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main(){
+  rep(i,1000) cout << "Hello World\n";
+  return 0;
+}
